4b0f3ba1364fea4b18b8ed07fcac359d14695b12
XWG <1576769920@QQ.COM>
2023-03-27 22:57:25 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o4G_P6hrH8AcQOPZHWuW6dYKEsvw',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'TWFMTcKhubzy_66ffpwZuDUkjsGXBuWQShShiRNxmhg',
+      useTemplateId: 'RFDhE7qagIyubS92v77y5r76f4CKKhUsXtJRP8PwcDs',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '06-14',
       festivals: [
