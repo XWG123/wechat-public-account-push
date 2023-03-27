b60fef4654388798d8898aae7ee4f37668674828
XWG <1576769920@QQ.COM>
2023-03-27 22:26:56 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o4G_P6hrH8AcQOPZHWuW6dYKEsvw',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'LJyxmWE6Te5hsJj4QTCW2hV7xbL3YEUmoclvSKAvxp4',
+      useTemplateId: 'DF5C3QLNZLL9LLZVOxnaj_gn71S-FAFKvQdHUW-P9TQ',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '06-14',
       festivals: [
@@ -32,11 +32,11 @@ const USER_CONFIG = {
 //           type: '*生日', name: '宝贝', year: '1996', date: '09-09',
 //         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+//         {
+//           type: '生日', name: '李四', year: '1995', date: '06-14',
+//         },
         {
-          type: '生日', name: '李四', year: '1995', date: '06-14',
-        },
-        {
-          type: '节日', name: '相爱纪念日', year: '2017', date: '01-14',
+          type: '节日', name: '结婚纪念日', year: '2022', date: '10-25',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
